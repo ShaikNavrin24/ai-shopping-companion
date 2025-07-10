c3e83161f74b10b6e2857cac52a038b2e9cea001
ShaikNavrin24 <shaiknavrin24@gmail.com>
2025-07-10 11:17:14 +0530

    updated file

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -1,6 +1,7 @@
 import vision from '@google-cloud/vision'
-const client = new vision.ImageAnnotatorClient()
-
+const client = new vision.ImageAnnotatorClient({
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS)
+});
 export default async function handler(req, res) {
   const { imageBase64 } = req.body
   const [result] = await client.labelDetection({ image: { content: imageBase64 } })
